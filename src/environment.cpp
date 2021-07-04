e730ed904798abcc40b3a15cf0ac578763ad9a25
evaplusai <evadraganova@hotmail.com>
2021-07-04 14:57:26 -0400

    Replace with renderPointCloud for actual point cloud processing

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,13 +42,14 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // Create lidar sensor 
     Lidar* lidar = new Lidar(cars, 0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar ->scan();
-    renderRays(viewer,lidar->position,inputCloud);
+    //renderRays(viewer,lidar->position,inputCloud);
+    renderPointCloud(viewer, inputCloud, "inputCloud");
     // TODO:: Create point processor
   
 }
