6be8b3cbab142713cb2bb6d55e765076d75a86da
evaplusai <evadraganova@hotmail.com>
2021-07-31 18:49:40 -0400

    Extending RANSAC to Planes

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -66,6 +66,19 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     auto startTime = std::chrono::steady_clock::now();
 	pcl::PointIndices::Ptr inliers;
     // TODO:: Fill in this function to find inliers for the cloud.
+    std::unordered_set<int> inliers = RansacPlane(cloud, 10, 1.3);
+    typename pcl::PointCloud<PointT>::Ptr cloudInliers(new pcl::PointCloud<PointT>);
+    typename pcl::PointCloud<PointT>::Ptr cloudOutliers(new pcl::PointCloud<PointT>);
+
+    for (int index = 0; index < cloud->points.size(); index++) {
+   
+      if (inliers.count(index))
+        cloudInliers->points.push_back(cloud->points[index]);
+      else
+        cloudOutliers->points.push_back(cloud->points[index]);
+    }
+
+    //end ransac segment
 
     pcl::SACSegmentation<PointT> seg;
     pcl::PointIndices::Ptr inliers{new pcl::PointIndices};
@@ -167,4 +180,59 @@ std::vector<boost::filesystem::path> ProcessPointClouds<PointT>::streamPcd(std::
 
     return paths;
 
+}
+
+template<typename PointT>
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud,
+                                       int maxIterations,
+                                       float distanceTol) {
+  std::unordered_set<int> inliersResult;
+
+  srand(time(NULL));
+
+  for (int index = 0; index < maxIterations; index++) {
+
+    std::unordered_set<int> inliers;
+    while (inliers.size() < 3)
+      inliers.insert(rand() % (cloud->points.size()));
+
+    float x1, y1, x2, y2, z1, z2, x3, y3, z3;
+    auto itr = inliers.begin();
+    x1 = cloud->points[*itr].x;
+    y1 = cloud->points[*itr].y;
+    z1 = cloud->points[*itr].z;
+    itr++;
+    x2 = cloud->points[*itr].x;
+    y2 = cloud->points[*itr].y;
+    z2 = cloud->points[*itr].z;
+    itr++;
+    x3 = cloud->points[*itr].x;
+    y3 = cloud->points[*itr].y;
+    z3 = cloud->points[*itr].z;
+
+    float a = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+    float b = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+    float c = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+    float d = (a * x1 + b * y1 + c * z1);
+
+    for (int index = 0; index < cloud->points.size(); index++) {
+      if (inliers.count(index) > 0)
+        continue;
+
+      pcl::PointXYZI point = cloud->points[index];
+      float x4 = point.x;
+      float y4 = point.y;
+      float z4 = point.z;
+
+      float d = fabs(a * x4 + b * y4 + c * z4 + d) / sqrt(a * a + b * b + c * c);
+
+      if (d <= distanceTol)
+        inliers.insert(index);
+
+    }
+    if (inliers.size() > inliersResult.size()) {
+      inliersResult = inliers;
+    }
+  }
+  return inliersResult;
 }
